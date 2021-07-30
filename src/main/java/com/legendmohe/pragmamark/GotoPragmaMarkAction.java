2e002730f52e526beb54f30633e6d4cf159fd970
hexinyu <hexinyu@bigo.sg>
2021-07-30 17:03:51 +0800

    update 211.*

--- src/main/java/com/legendmohe/pragmamark/GotoPragmaMarkAction.java
+++ src/main/java/com/legendmohe/pragmamark/GotoPragmaMarkAction.java
@@ -1,6 +1,11 @@
 package com.legendmohe.pragmamark;
 
-import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.PopupAction;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
@@ -11,14 +16,26 @@ import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.UnfairTextRange;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiSubstitutor;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * asdasdasdasd
+ */
 public class GotoPragmaMarkAction extends AnAction implements DumbAware, PopupAction {
 
     public static final String PRAGMA_MARK_PREFIX = "////////";
@@ -39,16 +56,44 @@ public class GotoPragmaMarkAction extends AnAction implements DumbAware, PopupAc
             processor.executeCommand(
                     project,
                     () -> {
-                        Collection<PragmaMarkData> pragmaMarks = getCustomFoldingDescriptors(editor, project);
-                        if (pragmaMarks.size() > 0) {
-                            PragmaMarkListPopup regionsPopup = new PragmaMarkListPopup(pragmaMarks, editor, project);
-                            regionsPopup.show();
-                        } else {
-                            notifyCustomRegionsUnavailable(editor, project);
-                        }
+                        process(e, project, editor);
                     },
                     "goto pragma marks",
                     null);
+        }
+    }
+
+    private void process(AnActionEvent e, Project project, Editor editor) {
+        PsiFile data = e.getData(LangDataKeys.PSI_FILE);
+        @NotNull PsiElement[] children = data.getChildren();
+        for (PsiElement child : children) {
+            if (child instanceof PsiClass) {
+                PsiClass psiClass = (PsiClass) child;
+                printPsiClass(psiClass, editor);
+            }
+            System.out.println(child.toString());
+        }
+        Collection<PragmaMarkData> pragmaMarks = getCustomFoldingDescriptors(editor, project);
+        if (pragmaMarks.size() > 0) {
+            PragmaMarkListPopup regionsPopup = new PragmaMarkListPopup(pragmaMarks, editor, project);
+            regionsPopup.show();
+        } else {
+            notifyCustomRegionsUnavailable(editor, project);
+        }
+    }
+
+    private void printPsiClass(PsiClass psiClass, Editor editor) {
+        @NotNull PsiMethod[] allMethods = psiClass.getMethods();
+        @NotNull PsiField[] allFields = psiClass.getFields();
+        @NotNull PsiClass[] allInnerClasses = psiClass.getInnerClasses();
+        System.out.println("allMethods=" + Arrays.toString(allMethods));
+        System.out.println("allFields=" + Arrays.toString(allFields));
+        System.out.println("allInnerClasses=" + Arrays.toString(allInnerClasses));
+
+        for (PsiMethod allMethod : allMethods) {
+            TextRange textRange = allMethod.getTextRange();
+            String lineContent = editor.getDocument().getText(textRange).trim();
+            System.out.println(">>  " + lineContent);
         }
     }
 
